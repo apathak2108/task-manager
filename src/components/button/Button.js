1dc43d849d100aeac3ff9efdaff14078f65cc7a7
Anany Pathak <propanany@gmail.com>
2024-03-23 17:28:15 +0530

    user interface

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,7 +4,7 @@ import "./Button.css";
 function Button({ handleAdd }) {
   return (
     <div className="btn">
-      <button className="submit-btn" onClick={handleAdd}>
+      <button className="submit-btn" onClick={handleAdd} >
         Add Task
       </button>
     </div>
