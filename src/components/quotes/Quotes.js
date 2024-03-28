81ab4238a688d33436eab9e52075effc19367bb3
Anany Pathak <propanany@gmail.com>
2024-03-28 12:00:27 +0530

    Implemented a quote api to randomly display the quotes fetched from the API

---
+++ src/components/quotes/Quotes.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { json } from "react-router-dom";
+import "./Quotes.css";
+
+function Quotes() {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    fetchQuote();
+    const interval = setInterval(fetchQuote, 5000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const fetchQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const { text, author } = data[randomIndex];
+        const cleanAuthor = author.split(',')[0];
+        setQuote({ text, author: cleanAuthor });
+      })
+      .catch((err) => console.error(err));
+  };
+  return (
+    <div>
+      {
+        <blockquote>
+          <p>“{quote.text}”</p>
+          <p id="author-name"> ~ {quote.author}</p>
+        </blockquote>
+      }
+    </div>
+  );
+}
+
+export default Quotes;
--- src/pages/auth/Auth.js
+++ src/pages/auth/Auth.js
@@ -1,9 +1,5 @@
 import React from "react";
-import "./Auth.css";
 import Login from "./Login";
 import Register from "./Register";
 
-
-
-
-export {Login, Register};
+export { Login, Register };
